7525985d6a7bf175c1ad65bf7a25b60c787a48bd
Hugo Tausch <hugoskummerkasten@gmail.com>
2023-06-25 14:26:25 +0200

    gen list of all days

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import Counter
 import os
+import pandas as pd
 
 json_path = "json"
 json_commits_path = f"{json_path}/commits"
@@ -26,21 +27,30 @@ def load_github_data():
         # Writing to repos.json
         with open(f"{json_commits_path}/{repo_name}.json", "w") as f:
             json.dump(commits, f, ensure_ascii=False, indent=4)
-    """     for commit in commits:
-            commit_date_str = commit["commit"]["committer"]["date"]
-            commit_date = datetime.strptime(commit_date_str, '%Y-%m-%dT%H:%M:%SZ')
-            all_commit_dates.append(commit_date)
-            print(all_commit_dates)
-
-    dcounts = Counter(d[0] for d in all_commit_dates) """
 
 
 def prepare_data():
     all_commit_dates = []
+
     files = os.listdir(f"./{json_commits_path}")
     for file in files:
-        f = open(f"./{json_commits_path}/data.json")
-    #data = json.load(f)
+        f = open(f"./{json_commits_path}/{file}")
+        commits = json.load(f)
+        for commit in commits:
+            commit_date_str = commit["commit"]["committer"]["date"]
+            commit_date = datetime.strptime(commit_date_str, '%Y-%m-%dT%H:%M:%SZ')
+            all_commit_dates.append(commit_date)
+    all_commit_dates.sort()
+
+    # create list of all days
+    base = datetime.today()
+    date_list = [(base - timedelta(days=x), 0) for x in range(365)]
+
+
+
+    print(all_commit_dates)
+    #dcounts = Counter(d[0] for d in all_commit_dates)
+    #print(dcounts)
 
 
 if __name__ == '__main__':
