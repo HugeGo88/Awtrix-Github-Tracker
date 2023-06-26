97257e2c74d8186eef0b3e9c3091f59072236513
Hugo Tausch <hugoskummerkasten@gmail.com>
2023-06-26 11:00:46 +0200

    minor improvements

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import os
 from paho.mqtt import client as mqtt_client
 import time
 
+
 class Object:
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True)
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True)
+
 
 class awtrix_github:
     json_path = "json"
@@ -20,7 +22,7 @@ class awtrix_github:
         api_url = "https://api.github.com/users/hugego88/repos"
         response = requests.get(api_url)
         commit_response = ""
-        repos =  response.json()
+        repos = response.json()
 
         # Writing to repos.json
         with open(f"{self.json_path}/repos.json", "w") as f:
@@ -33,7 +35,7 @@ class awtrix_github:
             commit_url = commit_url + "?page="
             while True:
                 commit_response = requests.get(f"{commit_url}{page}")
-                if(commit_response.text == "[]"):
+                if (commit_response.text == "[]"):
                     break
                 commits = commit_response.json()
 
@@ -42,12 +44,11 @@ class awtrix_github:
                     json.dump(commits, f, ensure_ascii=False, indent=4)
                 page += 1
 
-
     def prepare_data(self):
         commit_dates = []
         # create list of all days
         base = datetime.today()
-        self.days = [(base - timedelta(days=x), 0) for x in range(365)]
+        self.days = [(base - timedelta(days=x), 0) for x in range(180)]
 
         files = os.listdir(f"./{self.json_commits_path}")
         for file in files:
@@ -55,50 +56,48 @@ class awtrix_github:
             commits = json.load(f)
             for commit in commits:
                 commit_date_str = commit["commit"]["committer"]["date"]
-                commit_date = datetime.strptime(commit_date_str, '%Y-%m-%dT%H:%M:%SZ')
+                commit_date = datetime.strptime(
+                    commit_date_str, '%Y-%m-%dT%H:%M:%SZ')
                 commit_dates.append(commit_date)
         commit_dates.sort()
 
         for commit_date in commit_dates:
             for i, day in enumerate(self.days):
-                if(day[0].date() == commit_date.date()):
-                    self.days[i] = (day[0], day[1] +1 )
-                    if(day[1] > self.max_commits):
+                if (day[0].date() == commit_date.date()):
+                    self.days[i] = (day[0], day[1] + 1)
+                    if (day[1] > self.max_commits):
                         self.max_commits = day[1]
 
         for i, day in enumerate(self.days):
-            if(day[1] != 0):
-                self.days[i] = (day[0], int(float(day[1])/float(self.max_commits)*155.0+100))
+            if (day[1] != 0):
+                self.days[i] = (day[0], int(
+                    float(day[1])/float(self.max_commits)*175.0+65))
 
     def create_json(self):
         self.app_data = Object()
         self.app_data.icon = 5251
         self.app_data.duration = 10
         self.app_data.draw = [Object()]
-        self.app_data.draw[0].dp = [31, 8, f"#0000FF"]
+        self.app_data.draw[0].dp = [31, 8, f"#FFFFFF"]
         self.app_data.draw.pop()
         j = 0
         offset = ((datetime.today()).weekday() + 5) % 7
         for i, day in enumerate(self.days):
-            if(i == 0):
+            if (i == 0):
                 self.app_data.draw.append(Object())
-                row = 7-((i+offset)%7)
+                row = 7-((i+offset) % 7)
                 column = 31-int((i+offset)/7)
-                self.app_data.draw[j].dp = [column, row, f"#0000FF"]
+                self.app_data.draw[j].dp = [column, row, f"#FFFFFF"]
                 j += 1
                 continue
-            if(day[1] != 0):
+            if (day[1] != 0):
                 self.app_data.draw.append(Object())
-                row = 7-((i+offset)%7)
+                row = 7-((i+offset) % 7)
                 column = 31-int((i+offset)/7)
                 self.app_data.draw[j].dp = [column, row, f"#00{day[1]:02X}00"]
                 j += 1
-                if(j >= 49):
-                    break
 
-    def send_mqtt_msg(self):
-        topic = "awtrix_6ff9b8/notify"
-        #topic = "awtrix_6ff9b8/custom/github"
+    def send_mqtt_msg(self, topic):
         result = self.client.publish(topic, self.app_data.toJSON())
         # result: [0, 1]
         status = result[0]
@@ -115,14 +114,14 @@ class awtrix_github:
         self.client.connect(broker, port)
         return self.client
 
+
 if __name__ == '__main__':
     awtrix = awtrix_github()
-    #awtrix.load_github_data()
+    # awtrix.load_github_data()
     awtrix.prepare_data()
     awtrix.create_json()
     awtrix.connect_mqtt()
-    awtrix.send_mqtt_msg()
+    awtrix.send_mqtt_msg("awtrix_6ff9b8/custom/github")
+    awtrix.send_mqtt_msg("awtrix_6ff9b8/notify")
 
     print("end")
-
-    
